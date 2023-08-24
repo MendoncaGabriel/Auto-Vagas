411251e4850095d62d1c6c85b52f32efc2175777
Gabriel Andrade <mendoncagabriel1997@gmail.com>
2023-08-24 14:00:01 -0300

    testeCommit

--- BuscarVagas.bat
+++ BuscarVagas.bat
@@ -1,6 +1,13 @@
 @echo off
-echo Iniciando o script Node.js em segundo plano...
+echo Iniciando o script de commit em segundo plano...
 echo.
 
 start /min "" "C:\Users\Gabriel\Documents\GitHub\Auto-Vagas\commite.bat"
+
+timeout /t 300 /nobreak > nul
+
+echo Aguardando 5 minutos...
+
 start /min node "C:\Users\Gabriel\Documents\GitHub\Auto-Vagas\index.js"
+
+echo Script concluído.
