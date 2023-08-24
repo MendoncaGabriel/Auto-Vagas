0da2281b4701f8cccc80239f228382e9e3a04d00
Gabriel Andrade <mendoncagabriel1997@gmail.com>
2023-08-24 13:59:07 -0300

    testeCommit

--- BuscarVagas.bat
+++ BuscarVagas.bat
@@ -2,4 +2,5 @@
 echo Iniciando o script Node.js em segundo plano...
 echo.
 
-start /min node "C:\Users\Gabriel\Documents\GitHub\Auto-Vagas\index.js"
+start /min "" "C:\Users\Gabriel\Documents\GitHub\Auto-Vagas\commite.bat"
+
--- commite.bat
+++ commite.bat
@@ -3,7 +3,7 @@ echo Iniciando o processo de commit e push automatizado
 
 :: Substitua as linhas abaixo com suas informações
 set REPO_PATH=C:\Users\Gabriel\Documents\GitHub\Auto-Vagas
-set COMMIT_MESSAGE=autoCommit
+set COMMIT_MESSAGE=testeCommit
 
 :: Navegue para o diretório do repositório
 cd %REPO_PATH%
